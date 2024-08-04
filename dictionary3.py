ca54f5b1991d9230a9d95f9c675b4dc006687224
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-04 12:50:31 +0530

    Add files via upload

---
+++ dictionary3.py
@@ -0,0 +1,10 @@
+student_dict = {
+    "12345": "John Smith",
+    "54321": "Jane Doe",
+    "98765": "Alice Lee",
+    "67890": "Michael Brown",
+    "24681": "Sarah Jones",
+    "14325": "Ram Mickle"
+}
+
+print(student_dict["14325"])
